dd1fc5ac2ca3e282872e9801c972bca659931323
KaiwenZhang <kevin.zhangkw@gmail.com>
2019-07-17 21:38:00 -0600

    Update 3_Longest_Substring_Without_Repeating_Characters.py

--- 3_Longest_Substring_Without_Repeating_Characters.py
+++ 3_Longest_Substring_Without_Repeating_Characters.py
@@ -1,42 +1,25 @@
-import string
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
         if len(s) < 2:
             return len(s)
         
-        # dictionary of the alphabet
-        # key: letter
-        # value: count of appearence
+        result = 0
         dic = {}
-        for i in range(129):
-            dic[chr(i)] = 0
-        print(dic)
-        # dic = dict.fromkeys(string.ascii_lowercase, 0)
         
-        # store the length of longest substring
-        result = 1
+        index = 0
+        start = 0
+        cur_len = 0
         
-        index_outter = 0
-        index_inner = 1
-        while index_outter < len(s):
-            dic[s[index_outter]] = index_outter
-            while index_inner < len(s):  
-                print(s[index_inner])
-                print(dic[s[index_inner]])
-                if dic[s[index_inner]] != 0:
-                    #print(str(index_inner) + " " + str(index_outter))
-                    result = max(result, index_inner - dic[s[index_inner]])
-                    dic[s[index_inner]] = index_inner
-                    break
-                elif index_inner == len(s) - 1:
-                    # print(str(index_inner) + " " + str(index_outter))
-                    result = max(result, index_inner - index_outter)
-                    return result
-                else:
-                    dic[s[index_inner]] = index_inner
-                    index_inner += 1
-            index_outter += 1
-            dic = dict.fromkeys(string.ascii_lowercase, 0)
+        while index < len(s):
+            letter = s[index]
+            if letter in dic and dic[letter] >= start:
+                start = dic[letter]
+                cur_len = index - dic[letter]
+            else:
+                cur_len += 1
+            dic[letter] = index
+            result = max(result, cur_len)
+            index += 1
 
         return result
